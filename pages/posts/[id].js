0b2b134823fd791e1e43f2fa06b8f667a2235b5b
ike76 <ike76@me.com>
2020-05-11 14:13:29 -0500

    initial

---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import React from "react";
+import Head from "next/head";
+import Layout from "../../components/Layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
+//
+//
+
+const Post = ({ postData }) => {
+  const { title, id, date, contentHtml } = postData;
+  console.log({ postData });
+  return (
+    <Layout>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+      </article>
+    </Layout>
+  );
+};
+
+export default Post;
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: { postData },
+  };
+}
