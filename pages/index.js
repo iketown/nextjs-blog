31bd315ff600f7c2a0347a2bc3267a28a63e0b3a
ike76 <ike76@me.com>
2020-05-11 15:08:38 -0500

    extra text

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,10 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>Hello my name is Brian</p>
         <p>
+          here is some additional shit i have to say, and its going to live in
+          the preview-branch
+        </p>
+        <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
